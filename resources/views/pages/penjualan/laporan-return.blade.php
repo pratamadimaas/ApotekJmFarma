7d38de7f980e4883fbc18c0696191bdc7b3b4a71
Dimas <dimasmg2017@gmail.com>
2025-12-14 22:10:39 +0800

    Update laporan return penjualan

--- resources/views/pages/penjualan/laporan-return.blade.php
+++ resources/views/pages/penjualan/laporan-return.blade.php
@@ -48,25 +48,6 @@
     </div>
 
     <!-- Summary Card -->
-    <div class="row mb-4">
-        <div class="col-md-4">
-            <div class="card border-left-danger shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
-                                Total Return</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">
-                                Rp {{ number_format($totalReturn ?? 0, 0, ',', '.') }}
-                            </div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="bi bi-currency-dollar fs-2 text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
         <div class="col-md-4">
             <div class="card border-left-warning shadow h-100 py-2">
                 <div class="card-body">
